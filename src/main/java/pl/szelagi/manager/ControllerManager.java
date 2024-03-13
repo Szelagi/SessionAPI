b5f63c94b664d5b2572e5463dc0e82280aec5af6
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-13 18:44:25 +0100

    Support getControllers by Class and cleanup

--- src/main/java/pl/szelagi/manager/ControllerManager.java
+++ src/main/java/pl/szelagi/manager/ControllerManager.java
@@ -6,26 +6,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import pl.szelagi.component.controller.Controller;
 import pl.szelagi.component.controller.event.ControllerStartEvent;
 import pl.szelagi.component.controller.event.ControllerStopEvent;
 import pl.szelagi.component.session.Session;
 import pl.szelagi.manager.compare.CompareController;
-import pl.szelagi.util.IncrementalGenerator;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ControllerManager {
-    private static final HashMap<String, Listener> controllerListenerMap = new HashMap<>();
-    private static final HashMap<String, ArrayList<Long>> controllerIdMap = new HashMap<>();
-    private final static IncrementalGenerator idGenerator = new IncrementalGenerator();
+    private static final HashMap<String, Listener> CONTROLLER_LISTENER_MAP = new HashMap<>();
+    private static final HashMap<String, ArrayList<Long>> ENABLE_CONTROLLER_ID_MAP = new HashMap<>();
     private static JavaPlugin plugin;
-
-    public static IncrementalGenerator getIdGenerator() {
-        return idGenerator;
-    }
 
     public static void initialize(JavaPlugin p) {
         plugin = p;
@@ -45,19 +40,15 @@ public class ControllerManager {
         Bukkit.getPluginManager().registerEvents(new MyControllerManager(), p);
     }
 
-    private static int countControllerCurrentEnable(String name) {
-        var ids = controllerIdMap.get(name);
+    private static int controllerEnableCount(String name) {
+        var ids = ENABLE_CONTROLLER_ID_MAP.get(name);
         if (ids == null) return 0;
         return ids.size();
     }
     public static void onControllerStart(Controller controller) {
         var name = controller.getName();
         var id = controller.getId();
-        var ids = controllerIdMap.get(name);
-        if (ids == null) {
-            ids = new ArrayList<>();
-            controllerIdMap.put(name, ids);
-        }
+        var ids = ENABLE_CONTROLLER_ID_MAP.computeIfAbsent(name, k -> new ArrayList<>());
         if (ids.contains(id)) return;
         if (ids.isEmpty()) {
             startControllerListener(controller);
@@ -67,63 +58,77 @@ public class ControllerManager {
     public static void onControllerStop(Controller controller) {
         var name = controller.getName();
         var id = controller.getId();
-        var ids = controllerIdMap.get(name);
-        if (ids == null) {
-            ids = new ArrayList<>();
-            controllerIdMap.put(name, ids);
-        }
+        var ids = ENABLE_CONTROLLER_ID_MAP.computeIfAbsent(name, k -> new ArrayList<>());
         if (!ids.contains(id)) return;
         ids.remove(id);
         if (ids.isEmpty()) {
             stopControllerListener(controller);
-            controllerIdMap.remove(name); // added
+            ENABLE_CONTROLLER_ID_MAP.remove(name); // added
         }
     }
     private static void startControllerListener(Controller controller) {
         var name = controller.getName();
-        var listener = controllerListenerMap.get(name);
+        var listener = CONTROLLER_LISTENER_MAP.get(name);
         if (listener == null) {
             listener = controller.getListener();
             if (listener == null) return;
-            controllerListenerMap.put(name, listener);
+            CONTROLLER_LISTENER_MAP.put(name, listener);
         }
         Bukkit.getPluginManager().registerEvents(listener, plugin);
     }
     private static void stopControllerListener(Controller controller) {
         var name = controller.getName();
-        var listener = controllerListenerMap.get(name);
+        var listener = CONTROLLER_LISTENER_MAP.get(name);
         if (listener == null) return;
         HandlerList.unregisterAll(listener);
-        controllerListenerMap.remove(name); // delete listener when is disable
+        CONTROLLER_LISTENER_MAP.remove(name); // delete listener when is disable
     }
-    // methods // problem getMainProcess
-    @Nonnull
-    public static <T extends Controller> ArrayList<T> getControllers(Session d, CompareController cp) {
+
+    // getControllers
+    @NotNull
+    public static <T extends Controller> ArrayList<T> getControllers(Session session, CompareController cp) {
         var out = new ArrayList<T>();
-        if (d == null) return out;
-        for (var c : d.getMainProcess().getControllers()) {
-            if (cp.compare(c)) out.add((T) c);
+        if (session == null) return out;
+        for (var c : session.getMainProcess().getControllers()) {
+            if (cp.compare(c))
+                out.add((T) c);
         }
         return out;
     }
-    @Nonnull
-    public static <T extends Controller> ArrayList<T> getControllers(Player p, CompareController cp) {
-        var d = SessionManager.getSession(p);
-        if (d == null) return new ArrayList<>();
-        return getControllers(d, cp);
-    }
     @Nullable
-    public static <T extends Controller> T getFirstController(Session d, CompareController cp) {
-        if (d == null) return null;
-        for (var c : d.getMainProcess().getControllers()) {
-            if (cp.compare(c)) return (T) c;
+    public static <T extends Controller> T getFirstController(Session session, CompareController cp) {
+        if (session == null) return null;
+        for (var c : session.getMainProcess().getControllers()) {
+            if (cp.compare(c))
+                return (T) c;
         }
         return null;
     }
+
+    // Player
+    @NotNull
+    public static <T extends Controller> ArrayList<T> getControllers(Player player, CompareController cp) {
+        var d = SessionManager.getSession(player);
+        if (d == null) return new ArrayList<>();
+        return getControllers(d, cp);
+    }
+
     @Nullable
-    public static <T extends Controller> T getFirstController(Player p, CompareController cp) {
-        var d = SessionManager.getSession(p);
+    public static <T extends Controller> T getFirstController(Player player, CompareController cp) {
+        var d = SessionManager.getSession(player);
         if (d == null) return null;
         return getFirstController(d, cp);
     }
+
+    // Class
+    @NotNull
+    public static <T extends Controller> ArrayList<T> getControllers(Session session, Class<?> classType) {
+        return getControllers(session, classType::isInstance);
+    }
+
+    @Nullable
+    public static <T extends Controller> T getFirstController(Session session, Class<?> classType) {
+        return getFirstController(session, classType::isInstance);
+    }
+
 }
