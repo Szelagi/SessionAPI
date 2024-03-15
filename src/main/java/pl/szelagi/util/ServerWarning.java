7553ab76dd9de701735b3f891248e01a0b5b6963
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-15 02:46:25 +0100

    Fix doubling message server warning

--- src/main/java/pl/szelagi/util/ServerWarning.java
+++ src/main/java/pl/szelagi/util/ServerWarning.java
@@ -5,15 +5,13 @@ import org.bukkit.entity.Player;
 
 public class ServerWarning {
     private static void showWaring(String error) {
-        String errorMessage = "§6[ServerWarning] §f" + error;
+        String errorMessage = "§6[Warning] §f" + error;
         for (Player player : Bukkit.getServer().getOnlinePlayers())
             if (player.isOp())
                 player.sendMessage(errorMessage);
         Bukkit.getServer().getConsoleSender().sendMessage(errorMessage);
     }
     public ServerWarning(String message) {
-        for (var p : Bukkit.getServer().getOnlinePlayers())
-            if (p.isOp())
-                showWaring(message);
+        showWaring(message);
     }
 }
