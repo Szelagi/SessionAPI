07577bfe5738176fef16ddc63d22b175ed4763d5
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-16 01:52:09 +0100

    Multi start/stop controller exceptions

---
+++ src/main/java/pl/szelagi/component/baseexception/multi/MultiStartException.java
@@ -0,0 +1,11 @@
+package pl.szelagi.component.baseexception.multi;
+
+import pl.szelagi.component.BaseComponent;
+import pl.szelagi.component.ISessionComponent;
+import pl.szelagi.component.baseexception.StartException;
+
+public class MultiStartException extends StartException {
+    public MultiStartException(ISessionComponent component) {
+        super(component.getName() + ", " + BaseComponent.toType(component).name());
+    }
+}
