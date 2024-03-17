698c3904195a726ade9cd1b22aac496c974da983
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-17 23:56:01 +0100

    Use method from enum

--- src/main/java/pl/szelagi/component/baseexception/multi/MultiStartException.java
+++ src/main/java/pl/szelagi/component/baseexception/multi/MultiStartException.java
@@ -1,11 +1,11 @@
 package pl.szelagi.component.baseexception.multi;
 
-import pl.szelagi.component.BaseComponent;
+import pl.szelagi.component.ComponentType;
 import pl.szelagi.component.ISessionComponent;
 import pl.szelagi.component.baseexception.StartException;
 
 public class MultiStartException extends StartException {
 	public MultiStartException(ISessionComponent component) {
-		super(component.getName() + ", " + BaseComponent.toType(component).name());
+		super(component.getName() + ", " + ComponentType.toType(component).name());
 	}
 }
--- src/main/java/pl/szelagi/component/baseexception/multi/MultiStopException.java
+++ src/main/java/pl/szelagi/component/baseexception/multi/MultiStopException.java
@@ -1,11 +1,11 @@
 package pl.szelagi.component.baseexception.multi;
 
-import pl.szelagi.component.BaseComponent;
+import pl.szelagi.component.ComponentType;
 import pl.szelagi.component.ISessionComponent;
 import pl.szelagi.component.baseexception.StartException;
 
 public class MultiStopException extends StartException {
 	public MultiStopException(ISessionComponent component) {
-		super(component.getName() + ", " + BaseComponent.toType(component).name());
+		super(component.getName() + ", " + ComponentType.toType(component).name());
 	}
 }
