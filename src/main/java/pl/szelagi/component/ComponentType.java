dc07f1b7e7a79fefa02891c790eb85407d0fa095
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-18 19:36:15 +0100

    Remove type "other"

--- src/main/java/pl/szelagi/component/ComponentType.java
+++ src/main/java/pl/szelagi/component/ComponentType.java
@@ -5,7 +5,7 @@ import pl.szelagi.component.controller.Controller;
 import pl.szelagi.component.session.Session;
 
 public enum ComponentType {
-	SESSION, BOARD, CONTROLLER, OTHER;
+	SESSION, BOARD, CONTROLLER;
 
 	public static ComponentType toType(ISessionComponent component) {
 		if (component instanceof Controller)
@@ -14,6 +14,6 @@ public enum ComponentType {
 			return ComponentType.BOARD;
 		if (component instanceof Session)
 			return ComponentType.SESSION;
-		return ComponentType.OTHER;
+		throw new RuntimeException("unknown type of component");
 	}
 }
