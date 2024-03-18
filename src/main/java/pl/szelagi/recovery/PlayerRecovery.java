f6066b4e1e94767ea846cd8b25dd915e1fca926a
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-18 19:19:47 +0100

    PlayerRecovery support get listeners from event

--- src/main/java/pl/szelagi/recovery/PlayerRecovery.java
+++ src/main/java/pl/szelagi/recovery/PlayerRecovery.java
@@ -2,14 +2,12 @@ package pl.szelagi.recovery;
 
 import org.bukkit.entity.Player;
 import pl.szelagi.component.constructor.PlayerDestructorLambda;
-import pl.szelagi.component.constructor.UninitializedType;
+import pl.szelagi.event.player.recovery.PlayerRecoveryEvent;
 import pl.szelagi.manager.SessionManager;
-import pl.szelagi.process.MainProcess;
 import pl.szelagi.recovery.exception.RecoveryException;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public final class PlayerRecovery implements Serializable {
 	private final ArrayList<PlayerDestructorLambda> destructors;
@@ -17,26 +15,21 @@ public final class PlayerRecovery implements Serializable {
 	private final String playerAccountName;
 
 	public PlayerRecovery(Player player) throws RecoveryException {
-		var dungeon = SessionManager.getSession(player);
-		if (dungeon == null)
+		var session = SessionManager.getSession(player);
+		if (session == null)
 			throw new RecoveryException("player is not in session");
-		var playerDestructors = getControllerPlayerDestructor(player, dungeon.getMainProcess());
 
-		playerDestructors.addAll(dungeon.getCurrentBoard().getPlayerDestructorRecovery(player).getLambdas());
-		playerDestructors.addAll(dungeon.getPlayerDestructorRecovery(player).getLambdas());
+		var event = new PlayerRecoveryEvent(player);
+		session.getMainProcess()
+		       .invokeReverseAllListeners(event);
 
-		this.destructors = playerDestructors;
+		var lambdas = event.getLambdas()
+		                   .getLambdas();
+
+		var cloneLambdas = new ArrayList<>(lambdas);
+
+		this.destructors = cloneLambdas;
 		this.playerAccountName = player.getName();
-	}
-
-	private static ArrayList<PlayerDestructorLambda> getControllerPlayerDestructor(Player player, MainProcess mainProcess) {
-		var destructors = new ArrayList<PlayerDestructorLambda>();
-		for (var c : mainProcess.getControllers()) {
-			var localDestructors = c.getPlayerDestructorRecovery(player);
-			destructors.addAll(localDestructors.getLambdas());
-		}
-		Collections.reverse(destructors);
-		return destructors;
 	}
 
 	public ArrayList<PlayerDestructorLambda> getDestructors() {
@@ -47,7 +40,7 @@ public final class PlayerRecovery implements Serializable {
 		if (!playerAccountName.equals(player.getName()))
 			throw new RecoveryException("player name is other than saved!");
 		for (var destructor : destructors) {
-			destructor.run(player, UninitializedType.RECOVERY_DESTRUCTOR);
+			destructor.run(player);
 		}
 	}
 }
