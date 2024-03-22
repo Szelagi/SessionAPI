d385a49128cedf174576a8b256f87a32f08d18d0
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-22 23:44:08 +0100

    Lobby invoke sounds

--- src/main/java/pl/szelagi/buildin/lobby/Lobby.java
+++ src/main/java/pl/szelagi/buildin/lobby/Lobby.java
@@ -1,23 +1,29 @@
 package pl.szelagi.buildin.lobby;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 import org.bukkit.Location;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.Nullable;
+import pl.szelagi.cancelable.CancelCause;
 import pl.szelagi.component.ISessionComponent;
 import pl.szelagi.component.controller.Controller;
 import pl.szelagi.event.component.ComponentConstructorEvent;
+import pl.szelagi.event.player.canchange.PlayerCanJoinEvent;
 import pl.szelagi.event.player.initialize.PlayerConstructorEvent;
 import pl.szelagi.event.player.initialize.PlayerDestructorEvent;
 import pl.szelagi.util.event.Event;
 import pl.szelagi.util.timespigot.Time;
 
 public class Lobby extends Controller {
+	private static final Sound BREAK_SOUND = Sound.sound(Key.key("block" + ".note_block.bit"), Sound.Source.AMBIENT, 1f, .5f);
 	private boolean isLobby = true;
 	private final Location lobby;
 	private final int maxSlots;
 	private final int minSlots;
 	private final Time waitTime;
-	private Event<Runnable> finalizeEvent = new Event<>();
+	private final Event<Runnable> finalizeEvent = new Event<>();
 	private MessageTimer messageTimer;
 
 	public Lobby(ISessionComponent sessionComponent, Time waitTime, Location lobby, int maxSlots, int minSlots) {
@@ -50,6 +56,15 @@ public class Lobby extends Controller {
 		messageTimer.setBreakCountMessage("§cWe don't have enough players! Start cancelled.");
 		messageTimer.getFinalizeEvent()
 		            .bind(this::lobbyFinalize);
+	}
+
+	@Override
+	public void playerCanJoin(PlayerCanJoinEvent event) {
+		super.playerCanJoin(event);
+		if (maxSlots < event.getCurrentPlayers()
+		                    .size())
+			return;
+		event.setCanceled(new CancelCause(this, "lobby is full"));
 	}
 
 	@Override
@@ -95,6 +110,7 @@ public class Lobby extends Controller {
 		if (!isCounting())
 			return;
 		messageTimer.stop();
+		broadcastSound(BREAK_SOUND);
 	}
 
 	public boolean isCounting() {
@@ -119,5 +135,11 @@ public class Lobby extends Controller {
 	private void lobbyFinalize() {
 		isLobby = false;
 		finalizeEvent.call(Runnable::run);
+	}
+
+	private void broadcastSound(Sound sound) {
+		for (var player : getSession().getPlayers())
+			Audience.audience(player)
+			        .playSound(sound);
 	}
 }
