cbe1dd711ef8f5934a52bcd6aefeb228021907e5
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-24 23:42:38 +0100

    Refactor PlayerContainer

--- src/main/java/pl/szelagi/state/PlayerContainer.java
+++ src/main/java/pl/szelagi/state/PlayerContainer.java
@@ -2,34 +2,12 @@ package pl.szelagi.state;
 
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import pl.szelagi.state.manual.ManualContainerException;
-import pl.szelagi.state.manual.PlayerManualContainer;
 
 import java.io.Serializable;
-import java.util.function.Predicate;
+import java.util.function.Function;
 
-public class PlayerContainer<T extends PlayerState> implements Serializable {
-	private final PlayerManualContainer<T> manualContainer;
-	private final InstanceCreator<T> creator;
-
-	public PlayerContainer(InstanceCreator<T> creator) {
-		this.creator = creator;
-		this.manualContainer = new PlayerManualContainer<>();
-	}
-
-	public @NotNull T get(Player player) throws ManualContainerException {
-		if (manualContainer.isExists(player))
-			return manualContainer.get(player);
-		return manualContainer.create(player, creator);
-	}
-
-	public void clearPlayer(Player player) throws ManualContainerException {
-		if (manualContainer.isExists(player))
-			manualContainer.remove(player);
-	}
-
-	public @Nullable T find(Predicate<T> predicate) {
-		return manualContainer.find(predicate);
+public class PlayerContainer<T extends PlayerState> extends Container<Player, T> implements Serializable {
+	public PlayerContainer(@NotNull Function<Player, T> creator) {
+		super(creator);
 	}
 }
