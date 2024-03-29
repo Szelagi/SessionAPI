44911c6b0c543d4cd3b8cf08f4d97215dd766b79
Szelagi <kamil.szelagiewicz@gmail.com>
2024-03-29 23:58:22 +0100

    build-in RandomSlot allocator

---
+++ src/main/java/pl/szelagi/buildin/grouper/RandomSlot.java
@@ -0,0 +1,39 @@
+package pl.szelagi.buildin.grouper;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RandomSlot implements Iterator<Integer>, Iterable<Integer> {
+	private final ArrayList<Integer> free = new ArrayList<>();
+
+	public RandomSlot(int slotCount) {
+		for (int i = 0; i < slotCount; i++)
+			free.add(i);
+	}
+
+	public RandomSlot(int min, int maxExcluding) {
+		for (int i = min; i < maxExcluding; i++)
+			free.add(i);
+	}
+
+	public boolean hasNext() {
+		return !free.isEmpty();
+	}
+
+	public Integer next() {
+		if (!hasNext())
+			throw new RuntimeException("all slots are allocated");
+		int index = (int) (Math.random() * free.size());
+		var slot = free.get(index);
+		free.remove(index);
+		return slot;
+	}
+
+	@NotNull
+	@Override
+	public Iterator<Integer> iterator() {
+		return this;
+	}
+}
