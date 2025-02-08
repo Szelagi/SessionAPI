899fc14fff5449f38b3c83db2dab7f7abf4ef6a9
Kamil Szelągiewicz <kamil.szelagiewicz@gmail.com>
2025-02-08 20:26:33 +0100

    Better TagQuery metod names

--- src/main/java/pl/szelagi/tag/TagQuery.java
+++ src/main/java/pl/szelagi/tag/TagQuery.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -25,21 +26,54 @@ public class TagQuery extends ArrayList<Tag> implements Serializable {
 		this.tag = tag;
 	}
 
+	@Deprecated
 	public @NotNull String getQueryName() {
 		return tag;
 	}
 
+	public @NotNull String name() {
+		return tag;
+	}
+
+	@Deprecated
 	public @NotNull Tag getFirst() {
 		return get(0);
 	}
 
+	public @NotNull Tag first() throws NoSuchElementException {
+		if (size() == 0) throw new NoSuchElementException();
+		return get(0);
+	}
+
+	@Deprecated
 	public @NotNull Location getFirstLocation() {
 		return get(0).getLocation();
 	}
 
+	public @NotNull Location firstLocation() throws NoSuchElementException {
+		if (size() == 0) throw new NoSuchElementException();
+		return get(0).location();
+	}
+
+	public @NotNull Location firstCentredXZ() throws NoSuchElementException {
+		if (size() == 0) throw new NoSuchElementException();
+		return get(0).centeredXZ();
+	}
+
+	public @NotNull Location firstCentredXYZ() throws NoSuchElementException {
+		if (size() == 0) throw new NoSuchElementException();
+		return get(0).centeredXYZ();
+	}
+
+	@Deprecated
 	public @NotNull List<Location> toLocations() {
-		return stream().map(Tag::getLocation)
+		return stream().map(Tag::location)
 		               .toList();
+	}
+
+	public @NotNull List<Location> locations() {
+		return stream().map(Tag::location)
+				.toList();
 	}
 
 	public @NotNull <R> List<R> map(Function<Tag, R> mapper) {
